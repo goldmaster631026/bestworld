af24fece6b8f26a4fbe8be091563db4753997345
goldmaster631026 <goldmaster631026@gmail.com>
2024-09-19 16:07:16 +1000

    update portfolio

---
+++ src/components/molecules/Banner/index.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaDownload } from "react-icons/fa";
+import { RiFolderInfoFill } from "react-icons/ri";
+import TypeAnimation from "react-type-animation";
+import { motion } from "framer-motion";
+import Lottie from "react-lottie";
+import coding from "../../../assets/coding.json";
+import "../../../pages/shared/Shared.css";
+import { SecondaryBtn } from "../../../components";
+
+const Banner = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: coding,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div className="parent min-h-[100vh] flex flex-col-reverse lg:flex-row items-center justify-between">
+      <motion.div
+        initial={{ x: "-100vw" }}
+        animate={{ x: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <h2 className="text-neutral text-xl font-medium translate-y-[-90%] sm:translate-y-[-0%]">Hello, I'm </h2>
+        <h1 className="text-4xl font-semibold mb-0 translate-y-[-50%] sm:translate-y-[-0%]">Andres Romero</h1>
+        <div className="my-4">
+          <TypeAnimation
+            className="text-2xl text-primary font-bold translate-y-[-80%] sm:translate-y-[-0%]"
+            cursor={true}
+            sequence={[
+              "A Mern-stack Developer",
+              2019,
+              "A Back-end Developer",
+              2020,
+              "Web3 Developer",
+              2020,
+            ]}
+            wrapper="div"
+            repeat={Infinity}
+          />
+        </div>
+        <p className="text-neutral max-w-xl mb-6 font-medium translate-y-[-20%] sm:translate-y-[-0%]">
+          As a MERN stack developer, I am committed to building high-quality web
+          applications that meet the needs of my clients. With 4 years of
+          experience in full-stack web development, I specialize in using
+          HTML, CSS, React.js, Next js, MongoDB, Express.js, and Node.js to
+          create scalable and robust web applications.
+
+          I've worked as a Senior blockchain developer with 5 years of experience in React, Next.js, MongoDB, Web3, Solidity and Smart Contract.
+          I've developed many dApps, NFT market places, and games with blockchain.
+          I will show you some of my past and current projects to you.
+          I would be happier if you work with me, and I will start your project right now.
+          Hanks in advance.
+
+          {/* I am a Front-end Developer. I am very passionate to my work and
+          dedicated to explore New Tools And Technologies. */}
+        </p>
+
+        <div className="flex items-center translate-y-[-60%] sm:translate-y-[-0%]">
+          <a
+            href=""
+            target="blank"
+          >
+            <button className="primary-button">
+              <span>My Resume</span>
+              <span>
+                <FaDownload />
+              </span>
+            </button>
+          </a>
+          <Link to="/about" className="ml-4">
+            <SecondaryBtn>
+              <span>About Me</span>
+              <span>
+                <RiFolderInfoFill />
+              </span>
+            </SecondaryBtn>
+          </Link>
+        </div>
+      </motion.div>
+      <motion.div
+        className="w-full md:w-1/2"
+        initial={{ x: "100vw" }}
+        animate={{ x: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <Lottie options={defaultOptions} height="90%" width="90%" />
+      </motion.div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/pages/About/index.js
@@ -0,0 +1,140 @@
+import React from "react";
+import TypeAnimation from "react-type-animation";
+import { motion } from "framer-motion";
+import "./About.css";
+import "../../components/atoms/PrimaryBtn/PrimaryBtn.css";
+import "../shared/Shared.css";
+import { BottomLine, MySkill, Education } from "../../components";
+import { FaDownload } from "react-icons/fa";
+
+const About = () => {
+  return (
+    <div className="parent pt-16 my-16">
+      <div className="">
+        <motion.div
+          className="mb-10"
+          initial={{ y: -200, opacity: 0 }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: { duration: 1, type: "spring" },
+          }}
+        >
+          <h3 className="text-neutral text-center">Something About Myself</h3>
+          <h1 className="text-4xl font-semibold drop-shadow-md text-center">
+            About <span className="text-primary">Me</span>
+          </h1>
+          <BottomLine />
+        </motion.div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
+          <motion.div
+            initial={{ x: -200, opacity: 0 }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              transition: { duration: 1, delay: 1.25 },
+            }}
+          >
+            <img
+              src="1.jpeg" 
+              alt="Andres Romero"
+              className="p-12 w-[80%] h-50 transform translate-y-[-12%]"
+              title="Andres Romero"
+            />
+
+          </motion.div>
+          <motion.div
+            className=""
+            initial={{ x: 200, opacity: 0 }}
+            animate={{
+              x: 0,
+              opacity: 1,
+              transition: { duration: 1, delay: 1.25 },
+            }}
+          >
+            <h1 className="text-4xl font-semibold mb-4 text-center translate-y-[-380%] sm:translate-y-[-0%] sm:text-3xl sm:mb-2 md:text-left">Andres Romero</h1>
+            <div className="my-8">
+              <TypeAnimation
+                className="text-2xl text-primary font-bold text-center translate-y-[-500%] sm:translate-y-[-0%]  sm:text-2xl sm:mb-2 md:text-left"
+                cursor={true}
+                sequence={[
+                  "A Mern-stack Developer",
+                  2000,
+                  "A Full-stack Developer",
+                  2000,
+                  "A Front-end Developer",
+                  2000
+                ]}
+                wrapper="div"
+                repeat={Infinity}
+              />
+            </div>
+            <p className="text-neutral font-medium text-center translate-y-[-100%] sm:translate-y-[-0%] sm:mb-2 md:text-left">
+              As a MERN stack developer, I am committed to building high-quality
+              web applications that meet the needs of my clients. With years of
+              experience in full-stack web development, I specialize in using
+              React.js, Next js, Typescript, MongoDB, Express.js, and Node.js to
+              create scalable and robust web applications.
+
+              I've worked as a Senior blockchain developer with 5 years of experience in React, Next.js, MongoDB, Web3, Solidity and Smart Contract.
+              I've developed many dApps, NFT market places, and games with blockchain.
+              I will show you some of my past and current projects to you.
+              I would be happier if you work with me, and I will start your project right now.
+              hanks in advance.
+
+              {/* Dedicated Front-end developer. Capable to solve working problems.
+              Passionate about learning & development to reach the target. Eager
+              to tackle more complex problems and continue to find ways to
+              maximize user efficiency. */}
+            </p>
+            <br />
+            <p className="text-neutral font-medium text-center translate-y-[-150%] sm:translate-y-[-0%] sm:mb-2 md:text-left">
+              My passion for solving complex problems and creating innovative
+              solutions drives me to stay up-to-date with the latest
+              technologies and trends in the industry.
+              {/* My next mission is how to become a Mern stack Developer. I would
+              love to build some Giant Website which will shine myself. */}
+            </p>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-y-4 mt-4 text-center translate-y-[-50%] sm:translate-y-[-0%] sm:mb-2 md:text-left ">
+              <h2 className="font-medium">
+                <span className="mr-2 text-primary">Name : </span> Andres Romero
+              </h2>
+              <h2 className="font-medium">
+                <span className="mr-2 text-primary">Phone : </span>+5493884335452
+              </h2>
+              <h2 className="font-medium">
+                <span className="mr-2 text-primary">Email : </span>
+                goldmaster631026221@gmail.com
+              </h2>
+              {/* <h2 className="font-medium">
+                <span className="mr-2 text-primary">Address : </span>Canada , Montréal-Est
+              </h2> */}
+              <a
+                href=""
+                target="blank"
+              >
+                <div className="flex justify-center md:justify-start translate-y-[60%] sm:translate-y-[-0%]">
+                  <button className="primary-button">
+                    <span>My Resume</span>
+                    <span>
+                      <FaDownload />
+                    </span>
+                  </button>
+                </div>
+
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+
+      {/* My Skill */}
+      <MySkill />
+
+      {/* Education */}
+      <Education />
+    </div>
+  );
+};
+
+export default About;
