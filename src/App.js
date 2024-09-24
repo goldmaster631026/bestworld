d63593fa964bb552732c3c13b42eec8356925a8f
goldmaster631026 <goldmaster631026@gmail.com>
2024-09-24 17:24:02 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import About from "./pages/About";
 const Home = React.lazy(() => import("./pages/Home"));
 const Project = React.lazy(() => import("./pages/Project"));
 //app
+//update
 function App() {
   const location = useLocation();
   const isFalse = location.pathname.includes("404");
